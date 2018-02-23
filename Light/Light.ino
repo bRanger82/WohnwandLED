21c0321db38639daba48bb3e04eb16af12713cc6
Michael <bRanger82@users.noreply.github.com>
2018-02-23 13:52:42 +0100

    Update Light.ino

--- Light/Light.ino
+++ Light/Light.ino
@@ -3,18 +3,21 @@
 // the IR receiver is connected to PIN3
 #define RECV_PIN 2
 
-#define ERROR_LED 4
+// the PIN to which the temperature sensor is connected
+// http://funwidelectronics.blogspot.co.at/2016/10/lm35gui.html
+#define TP_PIN 5
 
-// defines the PIN where the temperature-sensor is connected
-#define TEMP_SENSOR 5
-// maximum temperature allowed
-#define TEMP_THRESHOLD 60
+#define DIAG_LED 4
+
+// PINOUT: https://camo.githubusercontent.com/081b569122da2244ff7de8bae15eb56947d05cc8/687474703a2f2f6472617a7a792e636f6d2f652f696d672f50696e6f7574543835612e6a7067
+
+float temperature=0.0f;
+const float MAX_TEMP = 60;
 
 // define two different outputs, so each output could be controlled seperatly (in this sketch both are controlled the same way)
 #define OUT_0 0
 #define OUT_1 1
 
-// defines the parameters of this program (start value for the outputs, in- and decrease steps, ...)
 #define CURR_START 0
 #define CURR_MAX 250
 #define CURR_MIN 0
@@ -25,7 +28,6 @@
 #define DELAY_STEP_UP 25
 #define DELAY_STEP_DW 25
 
-// defines the delay length for each loop() run 
 #define DELAY_LOOP_RUN 50
 
 // using library for receiving and processing IR signals
@@ -47,17 +49,19 @@ volatile bool runIt = false;
 volatile int curr = 0;
 
 // the setup function runs once when you press reset or power the board
-void setup() 
+void setup()
 {
   // activate IR receiver
   irrecv.enableIRIn(); // Start the receiver
-  
+
   // when turned on the value is 0
   curr = CURR_START;
   // Define pins, currently only OUT_1 is used
   pinMode(OUT_0, OUTPUT);
   pinMode(OUT_1, OUTPUT);
-  pinMode(ERROR_LED, OUTPUT);
+  pinMode(DIAG_LED, OUTPUT);
+
+  digitalWrite(DIAG_LED, LOW);
   analogWrite(OUT_0, curr);
   analogWrite(OUT_1, curr);
 }
@@ -69,7 +73,7 @@ void LightUpSlow()
 {
   // if a process is already running or the lights are already on max --> exit
   if (curr >= CURR_MAX || runIt) { return; }
-  
+
   runIt = true;
   while(curr<CURR_MAX)
   {
@@ -93,7 +97,7 @@ void LightDownSlow()
 {
   // if a process is already running or the lights are already on min --> exit
   if (curr <= CURR_MIN || runIt) { return; }
-  
+
   runIt = true;
   while(curr>CURR_MIN)
   {
@@ -110,72 +114,63 @@ void LightDownSlow()
   runIt = false;
 }
 
-/*
-  TESTING!!!
-  Gets the actual temperature value and if it is below the threshold value it returns true otherwise false
-  If no temperature sensor is available, connect the data pin to ground; in this case the function returns true
-*/
-bool IsTemperatureOK()
-{
-  float TempVal = analogRead(TEMP_SENSOR);
-
-  // no temperature sensor is connected (should not be done as this is a security feature, but someone might not want to have it ...)
-  if (TempVal == 0)
-  {
-    return true;
-  }
-  
-  TempVal = (TempVal / 255) * 5; // Converts analogRead value to Volts
-  // Voltage -> Temperature calculation, see https://learn.adafruit.com/tmp36-temperature-sensor
-  TempVal = (TempVal * 1000) - 500; 
-  TempVal = TempVal / 10;
-  
-  return (TempVal < TEMP_THRESHOLD);
-}
 
 // the loop function runs over and over again forever
-void loop() 
+void loop()
 {
   if (!IsTemperatureOK())
   {
-    analogWrite(OUT_0, 0);
-    analogWrite(OUT_1, 0);
-    digitalWrite(ERROR_LED, HIGH);
+    WriteToOutput(0);
+    digitalWrite(DIAG_LED, HIGH);
     delay(150);
-    digitalWrite(ERROR_LED, LOW);
-    delay(150);
+    digitalWrite(DIAG_LED, LOW);
+    delay(250);
     return;
   }
   
   // need a break, need a kitk ... ahm delay :)
   delay(DELAY_LOOP_RUN);
-
-  if (irrecv.decode(&results)) 
+  
+  if (irrecv.decode(&results))
   {
-    // based on the pressed button the desired funcion should be done
-    if (results.value == left) 
+    switch(results.value)
     {
-      LightDownSlow();
-    } else if (results.value == leftMid) 
-    {
-      curr -= CURR_STEP_DW;
-    } else if (results.value == rightMid) 
-    {
-      curr += CURR_STEP_UP;
-    } else if (results.value == right) 
-    {
-      LightUpSlow();
+      case left:
+        LightDownSlow();
+        break;
+      case leftMid:
+        curr -= CURR_STEP_DW;
+        WriteToOutput(curr);
+        break;
+      case rightMid:
+        curr += CURR_STEP_UP;
+        WriteToOutput(curr);
+        break;
+      case right:
+        LightUpSlow();
+        break;
+      default:
+        break;
     }
-
-    // Just to make sure that everything is in range
-    if (curr < CURR_MIN) { curr = CURR_MIN; }
-    if (curr > CURR_MAX) { curr = CURR_MAX; }
-    
-    // Output value to OUT_0 and OUT_1
-    analogWrite(OUT_0, curr);
-    analogWrite(OUT_1, curr);
-
     // ready for receiving the next value
     irrecv.resume(); // Receive the next value
   }
+}
+
+bool IsTemperatureOK()
+{
+  temperature = analogRead(TP_PIN);
+  temperature = (0.4887*temperature);
+  return (temperature <= MAX_TEMP)
+}
+
+void WriteToOutput(int val)
+{
+  // Just to make sure that everything is in range
+  if (val < CURR_MIN) { val = CURR_MIN; }
+  if (val > CURR_MAX) { val = CURR_MAX; }
+
+  // Output value to OUT_0 and OUT_1
+  analogWrite(OUT_0, val);
+  analogWrite(OUT_1, val);
 }
